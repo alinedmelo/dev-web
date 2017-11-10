b98aaded79a3cd28f3661f0a166ffa184aa16158
Aline Dias <aline.meldias@gmail.com>
2017-11-10 18:18:23 -0200

    Typescript - Arquivo de config, compilação e iferência de tipos

---
+++ angular/typescript/app.ts
@@ -0,0 +1,15 @@
+/* Inferência de tipos
+Permite definir o tipo de valores recebidos pelas variáveis
+tornando o JS uma linguagem fortemente tipada e evitando erros e sobreposição de valores
+*/
+
+let mensagem: string = 'Seja bem vinda!'
+let numCursos: number = 10
+let estudando: boolean = true
+
+// Declarando a tipagem de arrays
+let listaFrutas: Array<string> = ['Melão', 'Banana', 'Uva']
+let listaFrutas2: string[] = ['Melão', 'Banana', 'Uva']
+
+let listaNotas: Array<number> = [6, 8, 10]
+let listaNotas2: number[] = [6, 8, 10]
---
+++ angular/typescript/dist/app.js
@@ -0,0 +1,8 @@
+"use strict";
+var mensagem = 'Seja bem vinda!';
+var numCursos = 10;
+var estudando = true;
+var listaFrutas = ['Melão', 'Banana', 'Uva'];
+var listaFrutas2 = ['Melão', 'Banana', 'Uva'];
+var listaNotas = [6, 8, 10];
+var listaNotas2 = [6, 8, 10];
